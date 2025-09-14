06d7325feef3e8b842e2cf8a63cb9ce8469ba172
aoqia <aoqia@aoqia.dev>
2025-09-14 11:34:11 +1000

    feat: Add full entrypoint support for InitExtension

--- src/main/java/dev/aoqia/leaf/api/mixin/LuaManagerMixin.java
+++ src/main/java/dev/aoqia/leaf/api/mixin/LuaManagerMixin.java
@@ -10,18 +10,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import zombie.Lua.LuaManager;
 
-import static dev.aoqia.leaf.api.LeafApi.LOGGER;
-
 @Mixin(LuaManager.class)
 public class LuaManagerMixin {
     @Inject(method = "init", at = @At("TAIL"))
     private static void init(CallbackInfo ci) {
-        var entrypoints = LeafLoader.getInstance().getEntrypoints("main", InitExtension.class);
-        for (var e : entrypoints) {
-            if (e instanceof InitExtension) {
-                LOGGER.debugln("Calling onLuaInitialize for class " + e.getClass().getName());
-                e.onLuaInitialize();
-            }
-        }
+        final var loader = LeafLoader.getInstance();
+        loader.getEntrypoints("main", InitExtension.class)
+            .forEach(ModExt::onLuaInitialize);
+        loader.getEntrypoints("client", InitExtension.class)
+            .forEach(ModExt::onLuaInitialize);
+        loader.getEntrypoints("server", InitExtension.class)
+            .forEach(ModExt::onLuaInitialize);
     }
 }
